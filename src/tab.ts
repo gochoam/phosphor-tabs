b85c6048a6350f427f4121203b2f5e375452d410
S. Chris Colbert <sccolbert@gmail.com>
2015-09-08 17:34:39 -0400

    tabs -> spaces

--- src/tab.ts
+++ src/tab.ts
@@ -8,7 +8,7 @@
 'use strict';
 
 import {
-	NodeWrapper
+  NodeWrapper
 } from 'phosphor-nodewrapper';
 
 
@@ -54,80 +54,80 @@ const CLOSABLE_CLASS = 'p-mod-closable';
  */
 export
 class Tab extends NodeWrapper {
-	/**
-	 * Create the DOM node for a tab.
-	 */
-	static createNode(): HTMLElement {
-		var node = document.createElement('div');
-		var icon = document.createElement('span');
-		var text = document.createElement('span');
-		var closeIcon = document.createElement('span');
-		icon.className = ICON_CLASS;
-		text.className = TEXT_CLASS;
-		closeIcon.className = CLOSE_ICON_CLASS;
-		node.appendChild(icon);
-		node.appendChild(text);
-		node.appendChild(closeIcon);
-		return node;
-	}
+  /**
+   * Create the DOM node for a tab.
+   */
+  static createNode(): HTMLElement {
+    var node = document.createElement('div');
+    var icon = document.createElement('span');
+    var text = document.createElement('span');
+    var closeIcon = document.createElement('span');
+    icon.className = ICON_CLASS;
+    text.className = TEXT_CLASS;
+    closeIcon.className = CLOSE_ICON_CLASS;
+    node.appendChild(icon);
+    node.appendChild(text);
+    node.appendChild(closeIcon);
+    return node;
+  }
 
-	/**
-	 * Construct a new tab.
-	 *
-	 * @param text - The initial text for the tab.
-	 */
-	constructor(text?: string) {
-		super();
-		this.addClass(TAB_CLASS);
-		if (text) this.text = text;
-	}
+  /**
+   * Construct a new tab.
+   *
+   * @param text - The initial text for the tab.
+   */
+  constructor(text?: string) {
+    super();
+    this.addClass(TAB_CLASS);
+    if (text) this.text = text;
+  }
 
-	/**
-	 * Get the text for the tab.
-	 */
-	get text(): string {
-		return (<HTMLElement>this.node.children[1]).textContent;
-	}
+  /**
+   * Get the text for the tab.
+   */
+  get text(): string {
+    return (<HTMLElement>this.node.children[1]).textContent;
+  }
 
-	/**
-	 * Set the text for the tab.
-	 */
-	set text(text: string) {
-		(<HTMLElement>this.node.children[1]).textContent = text;
-	}
+  /**
+   * Set the text for the tab.
+   */
+  set text(text: string) {
+    (<HTMLElement>this.node.children[1]).textContent = text;
+  }
 
-	/**
-	 * Get whether the tab is selected.
-	 */
-	get selected(): boolean {
-		return this.hasClass(SELECTED_CLASS);
-	}
+  /**
+   * Get whether the tab is selected.
+   */
+  get selected(): boolean {
+    return this.hasClass(SELECTED_CLASS);
+  }
 
-	/**
-	 * Set whether the tab is selected.
-	 */
-	set selected(selected: boolean) {
-		this.toggleClass(SELECTED_CLASS, selected);
-	}
+  /**
+   * Set whether the tab is selected.
+   */
+  set selected(selected: boolean) {
+    this.toggleClass(SELECTED_CLASS, selected);
+  }
 
-	/**
-	 * Get whether the tab is closable.
-	 */
-	get closable(): boolean {
-		return this.hasClass(CLOSABLE_CLASS);
-	}
+  /**
+   * Get whether the tab is closable.
+   */
+  get closable(): boolean {
+    return this.hasClass(CLOSABLE_CLASS);
+  }
 
-	/**
-	 * Set whether the tab is closable.
-	 */
-	set closable(closable: boolean) {
-		this.toggleClass(CLOSABLE_CLASS, closable);
-	}
+  /**
+   * Set whether the tab is closable.
+   */
+  set closable(closable: boolean) {
+    this.toggleClass(CLOSABLE_CLASS, closable);
+  }
 
-	/**
-	 * Get the DOM node for the tab close icon.
-	 */
-	get closeIconNode(): HTMLElement {
-		return <HTMLElement>this.node.lastChild;
-	}
+  /**
+   * Get the DOM node for the tab close icon.
+   */
+  get closeIconNode(): HTMLElement {
+    return <HTMLElement>this.node.lastChild;
+  }
 }
